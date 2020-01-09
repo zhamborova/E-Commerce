294b9cfedcff384d9bc25e62220ecb2f01cbf684
yasmi <yasmi.zhamborova@mail.ru>
2020-01-09 16:29:32 +0300

    homepage components

---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './homepage.styles.scss'
+import Directory from "../../components/directory/directory.component";
+export const HomePage = () =>{
+
+ return <div className='homepage'>
+       <Directory/>
+       </div>
+};
