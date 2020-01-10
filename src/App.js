1763da209be0d13bf65089c8ba077a4a14d8c471
yasmi <yasmi.zhamborova@mail.ru>
2020-01-10 15:03:42 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom'
 import {HomePage} from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div >
-     <HomePage/>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop/hats' component={Hats}/>
     </div>
   );
 }
 
+const Hats=()=> <h2>HATS</h2>;
 export default App;
